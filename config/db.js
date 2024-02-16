f81cdbc3efc7d7c3485cd9b4d24860de291f6a63
Adriano Ayala <adriano.ayc@gmail.com>
2024-02-15 23:54:30 -0400

    Incrementando el maximo de coneccion de sequelize

--- config/db.js
+++ config/db.js
@@ -14,7 +14,7 @@ const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env
         timestamps: true
     },
     pool: {
-        max: 5,         // Maximo de conexiones
+        max: 10,         // Maximo de conexiones
         min: 0,         // Minimo de conexiones
         acquire: 30000, // Tiempo antes de marcar un error
         idle: 10000     // Tiempo que debe transcurrir para finalizar la conexion (liberar espacio)
